ca4815fd73ff3f14adb792c650fc89548e1008fa
Shaikh Muzaker <shaikhmuzaker@gmail.com>
2018-05-06 02:01:28 +0530

    Add files via upload

---
+++ src/assets/js/slide.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $(".divClass").slideDown("slow",function ()
+    {
+       $("#textMsg").text("");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $(".divClass").slideDown("slow",function ()
+    {
+       $("#textMsg").text("");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $(".divClass").slideDown("slow",function ()
+    {
+       $("#textMsg").text("");
+    });
+  });
+});
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $(".divClass").slideDown("slow",function ()
+    {
+       $("#textMsg").text("");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $(".divClass").slideDown("slow",function ()
+    {
+       $("#textMsg").text("");
+    });
+  });
+});
